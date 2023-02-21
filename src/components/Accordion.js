96957a9fc5d4afca8082bdfb040258345a387479
Kristin Lam <klam623@gmail.com>
2023-02-20 22:43:25 -0500

    Map through accordion items, add event listener

--- src/components/Accordion.js
+++ src/components/Accordion.js
@@ -1,10 +1,22 @@
+import { useState } from 'react';
+
 const Accordion = ({ items }) => {
-  const renderedItems = items.map((item) => (
-    <div key={item.id}>
-      <div>{item.label}</div>
-      <div>{item.content}</div>
-    </div>
-  ));
+  const [expandedIndex, setExpandedIndex] = useState(0);
+
+  const handleClick = (nextIndex) => {
+    setExpandedIndex(nextIndex);
+  };
+
+  const renderedItems = items.map((item, index) => {
+    const isExpanded = index === expandedIndex;
+
+    return (
+      <div key={item.id}>
+        <div onClick={() => handleClick(index)}>{item.label}</div>
+        {isExpanded && <div>{item.content}</div>}
+      </div>
+    );
+  });
 
   return <div>{renderedItems}</div>;
 };
