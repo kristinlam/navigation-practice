a04ca58ae40f2768175cf768e964fea7ea5d699d
Kristin Lam <klam623@gmail.com>
2023-02-22 23:08:39 -0500

    Move accordion to own page, create dropdown

---
+++ src/pages/AccordionPage.js
@@ -0,0 +1,24 @@
+import Accordion from '../components/Accordion';
+
+const AccordionPage = () => {
+  const items = [
+    {
+      id: 1,
+      label: 'What frontend frameworks are popular?',
+      content: 'React and Redux are popular frontend frameworks.',
+    },
+    {
+      id: 2,
+      label: 'What backend frameworks are popular?',
+      content: 'Express and Sequelize are popular backend frameworks.',
+    },
+    {
+      id: 3,
+      label: 'What frameworks should I learn next?',
+      content: 'TypeScript and Next.js both sound like good ideas.',
+    },
+  ];
+  return <Accordion items={items} />;
+};
+
+export default AccordionPage;
