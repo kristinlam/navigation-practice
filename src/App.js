019364731619358408bc3a4fcce09ef46b934b5b
Kristin Lam <klam623@gmail.com>
2023-02-25 19:01:02 -0500

    Add links and routes to test

--- src/App.js
+++ src/App.js
@@ -1,5 +1,25 @@
+import Link from './components/Link';
+import Route from './components/Route';
+import AccordionPage from './pages/AccordionPage';
+import DropdownPage from './pages/DropdownPage';
+
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <div>
+        <Link to="/accordion">Accordion</Link>
+        <Link to="/dropdown">Dropdown</Link>
+      </div>
+      <div>
+        <Route path="/accordion">
+          <AccordionPage />
+        </Route>
+        <Route path="/dropdown">
+          <DropdownPage />
+        </Route>
+      </div>
+    </div>
+  );
 };
 
 export default App;
