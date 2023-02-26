f1b51407f94404a7190b360925fec94adf1244b3
Kristin Lam <klam623@gmail.com>
2023-02-26 14:22:57 -0500

    Display modal with portal

--- src/components/Modal.js
+++ src/components/Modal.js
@@ -1,5 +1,16 @@
+import { createPortal } from 'react-dom';
+
 const Modal = () => {
-  return <div>Modal</div>;
+  return createPortal(
+    <div>
+      <div className="absolute inset-0 bg-gray-300 opacity-80"></div>
+      <div className="absolute inset-40 p-10 bg-white">
+        <p>Modal text here</p>
+      </div>
+    </div>,
+
+    document.querySelector('.modal-container')
+  );
 };
 
 export default Modal;
