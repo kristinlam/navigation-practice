f791ec8f2539725616899954cbc1bf66ec157320
Kristin Lam <klam623@gmail.com>
2023-02-26 01:52:51 -0500

    Setup and link to page for displaying modal

--- src/App.js
+++ src/App.js
@@ -3,20 +3,24 @@ import Route from './components/Route';
 import AccordionPage from './pages/AccordionPage';
 import ButtonPage from './pages/ButtonPage';
 import DropdownPage from './pages/DropdownPage';
+import ModalPage from './pages/ModalPage';
 
 const App = () => {
   return (
     <div className="container mx-auto grid grid-cols-6 gap-4 mt-4">
       <Sidebar />
       <div className="col-span-5">
+        <Route path="/">
+          <DropdownPage />
+        </Route>
         <Route path="/accordion">
           <AccordionPage />
         </Route>
         <Route path="/buttons">
           <ButtonPage />
         </Route>
-        <Route path="/">
-          <DropdownPage />
+        <Route path="/modal">
+          <ModalPage />
         </Route>
       </div>
     </div>
