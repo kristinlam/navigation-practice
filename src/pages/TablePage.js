9e464954c891d269dae372b205b372790b8325e8
Kristin Lam <klam623@gmail.com>
2023-05-01 18:39:05 -0400

    Make tables render dynamically

--- src/pages/TablePage.js
+++ src/pages/TablePage.js
@@ -8,11 +8,22 @@ const TablePage = () => {
     { name: 'Lime', color: 'bg-green-500', score: 4 },
   ];
 
-  const config = [{ label: 'Name' }, { label: 'Color' }, { label: 'Score' }];
+  const config = [
+    { label: 'Name', render: (fruit) => fruit.name },
+    {
+      label: 'Color',
+      render: (fruit) => <div className={`p-3 m-2 ${fruit.color}`} />,
+    },
+    { label: 'Score', render: (fruit) => fruit.score },
+  ];
+
+  const keyFn = (fruit) => {
+    return fruit.name;
+  };
 
   return (
     <div>
-      <Table data={data} config={config} />
+      <Table data={data} config={config} keyFn={keyFn} />
     </div>
   );
 };
